f9cb49caaf075fa2015b3af1cdd362695a90ed59
Kafka Wanna Fly <47100782+KafkaWannaFly@users.noreply.github.com>
2024-05-02 22:01:47 +0700

    Abtract test class

---
+++ UnitTest/BaseTest.cs
@@ -0,0 +1,85 @@
+using Microsoft.CodeAnalysis;
+
+namespace UnitTest;
+
+public class BaseTest<TGenerator> where TGenerator : IIncrementalGenerator, new()
+{
+    protected IIncrementalGenerator _generator = new TGenerator();
+
+    virtual protected string GetDataFolder()
+    {
+        return string.Empty;
+    }
+
+    [Fact]
+    public void ShouldDoNothing()
+    {
+        var sourceCode = File.ReadAllText(
+            Path.Combine(
+                Directory.GetCurrentDirectory(),
+                "TestData",
+                "Shared",
+                "NoUseAtAll.txt"
+            )
+        );
+
+        var generatedCode = TestHelper.GetGeneratedOutput(sourceCode, _generator);
+
+        generatedCode.Should().BeNullOrEmpty();
+    }
+
+    [Fact]
+    public void ShouldGenerateBasicCode()
+    {
+        var sourceCode = File.ReadAllText(
+            Path.Combine(
+                GetDataFolder(),
+                "SimpleUsage.txt"
+            )
+        );
+
+        var generatedCode = TestHelper.GetGeneratedOutput(sourceCode, _generator);
+        generatedCode = TestHelper.RemoveAllWhiteSpace(generatedCode ?? "");
+
+        var expectedCode = File.ReadAllText(
+            Path.Combine(
+                Directory.GetCurrentDirectory(),
+                GetDataFolder(),
+                "SimpleUsage.expected.txt"
+            )
+        );
+        expectedCode = TestHelper.RemoveAllWhiteSpace(expectedCode);
+
+        generatedCode.Should().NotBeNullOrEmpty()
+            .And
+            .BeEquivalentTo(expectedCode);
+    }
+
+    [Fact]
+    public void ShouldGenerateCodeWithFullSettings()
+    {
+        var sourceCode = File.ReadAllText(
+            Path.Combine(
+                Directory.GetCurrentDirectory(),
+                GetDataFolder(),
+                "FullSettings.txt"
+                )
+            );
+
+        var generatedCode = TestHelper.GetGeneratedOutput(sourceCode, _generator);
+        generatedCode = TestHelper.RemoveAllWhiteSpace(generatedCode ?? "");
+
+        var expectedCode = File.ReadAllText(
+            Path.Combine(
+                Directory.GetCurrentDirectory(),
+                GetDataFolder(),
+                "FullSettings.expected.txt"
+                )
+            );
+        expectedCode = TestHelper.RemoveAllWhiteSpace(expectedCode);
+
+        generatedCode.Should().NotBeNullOrEmpty()
+            .And
+            .BeEquivalentTo(expectedCode);
+    }
+}
--- UnitTest/BindableReadOnlyPropTest.cs
+++ UnitTest/BindableReadOnlyPropTest.cs
@@ -2,57 +2,10 @@
 
 namespace UnitTest;
 
-public class BindableReadOnlyPropTest
+public class BindableReadOnlyPropTest : BaseTest<BindableReadOnlyPropSG>
 {
-    private readonly IIncrementalGenerator _generator = new BindableReadOnlyPropSG();
-
-    private readonly string _testFolder = Path.Combine(
-        Directory.GetCurrentDirectory(),
-        "TestData",
-        "BindableReadOnlyProp"
-    );
-
-    [Fact]
-    public void ShouldDoNothing()
+    protected override string GetDataFolder()
     {
-        var sourceCode = File.ReadAllText(
-            Path.Combine(
-                Directory.GetCurrentDirectory(),
-                "TestData",
-                "Shared",
-                "NoUseAtAll.txt"
-            )
-        );
-
-        var generatedCode = TestHelper.GetGeneratedOutput(sourceCode, _generator);
-
-        generatedCode.Should().BeNullOrEmpty();
-    }
-
-    [Fact]
-    public void ShouldGenerateBasicCode()
-    {
-        var sourceCode = File.ReadAllText(
-            Path.Combine(
-                _testFolder,
-                "SimpleUsage.txt"
-            )
-        );
-
-        var generatedCode = TestHelper.GetGeneratedOutput(sourceCode, _generator);
-        generatedCode = TestHelper.RemoveAllWhiteSpace(generatedCode ?? "");
-
-        var expectedCode = File.ReadAllText(
-            Path.Combine(
-                Directory.GetCurrentDirectory(),
-                _testFolder,
-                "SimpleUsage.expected.txt"
-            )
-        );
-        expectedCode = TestHelper.RemoveAllWhiteSpace(expectedCode);
-
-        generatedCode.Should().NotBeNullOrEmpty()
-            .And
-            .BeEquivalentTo(expectedCode);
+        return "TestData/BindableReadOnlyProp";
     }
 }
