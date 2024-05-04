964c1fc83bce46e3f6d9993c52b2ce75d4033892
Kafka Wanna Fly <47100782+KafkaWannaFly@users.noreply.github.com>
2024-05-04 15:56:58 +0700

    Clean up code

--- BindableProps/AttachedProp.cs
+++ BindableProps/AttachedProp.cs
@@ -5,17 +5,4 @@
 /// </summary>
 [AttributeUsage(AttributeTargets.Field)]
 // ReSharper disable once UnusedType.Global
-public sealed class AttachedProp : Attribute
-{
-    public int DefaultBindingMode { get; set; }
-
-    public string? ValidateValueDelegate { get; set; }
-
-    public string? PropertyChangedDelegate { get; set; }
-
-    public string? PropertyChangingDelegate { get; set; }
-
-    public string? CoerceValueDelegate { get; set; }
-
-    public string? CreateDefaultValueDelegate { get; set; }
-}
+public class AttachedProp : BindableProp;
